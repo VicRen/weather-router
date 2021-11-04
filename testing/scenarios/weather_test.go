b6ebc34e361208042b57b59c908fc259c2c5b309
VicRen <vic.ren@me.com>
2021-11-04 19:43:07 +0800

    Update weather_test.go

--- testing/scenarios/weather_test.go
+++ testing/scenarios/weather_test.go
@@ -1,7 +1,23 @@
 package scenarios
 
-import "testing"
+import (
+	"testing"
+	"weather-router/internal/data"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
+
+type dummyLogger struct {
+}
+
+func (d *dummyLogger) Log(level log.Level, keyvals ...interface{}) error {
+	return nil
+}
 
 func TestWeatherGetNow(t *testing.T) {
-
+	_, cleanup, err := data.NewHeWeather(nil)
+	defer cleanup()
+	if err != nil {
+		return
+	}
 }

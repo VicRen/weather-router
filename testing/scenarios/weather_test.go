91acb20e426b90ed6de75db6521b130fdda0998b
VicRen <vic.ren@me.com>
2021-11-09 09:39:53 +0800

    Update weather_test.go

--- testing/scenarios/weather_test.go
+++ testing/scenarios/weather_test.go
@@ -1,14 +1,28 @@
 package scenarios
 
 import (
+	"net"
 	"testing"
+	"weather-router/internal/biz"
 	"weather-router/internal/data"
+	"weather-router/internal/server"
+	"weather-router/internal/service"
 )
 
 func TestWeatherGetNow(t *testing.T) {
-	_, cleanup, err := data.NewHeWeather(&dummyLogger{})
+	logger := &dummyLogger{}
+	heweather, cleanup, err := data.NewHeWeather(logger)
 	defer cleanup()
 	if err != nil {
 		return
 	}
+	weatherRepo := data.NewWeatherRepo(heweather, logger)
+	getNowWeatherUsecase := biz.NewGetNowWeatherUsecase(weatherRepo, logger)
+	weatherService := service.NewWeatherService(getNowWeatherUsecase, logger)
+	httpServer := server.NewHTTPServer(nil, weatherService, logger)
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		return
+	}
+	httpServer.Serve(l)
 }
